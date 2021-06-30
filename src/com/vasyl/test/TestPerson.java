42c7d7244396118c908dcb4e0e4c90204e446f3f
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-30 10:16:53 -0400

    Compare the differences betwen abstract classes and the inheritance mechanism in Java

---
+++ src/com/vasyl/test/TestPerson.java
@@ -0,0 +1,15 @@
+package com.vasyl.test;
+
+import com.vasyl.Person;
+
+public class TestPerson {
+	
+	public static void main(String args) {
+	//  you can't instanciate from abstarct class
+		
+    //  Person adam = new Person();
+		
+	// Main reason is to provide a template for other classes to start from
+	}
+
+}
